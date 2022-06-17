9ea9d796fe5ee339a23dc99726104d1283e3a854
Dejan Kostenarov <87342103+ElderLearningDenz@users.noreply.github.com>
2022-06-17 19:28:02 +0200

    homework01

---
+++ Homework01/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("ListOrders")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult JsonData()
+        {
+            Order orderOne = new Order()
+            {
+                Id = 1,
+                Name = "Bryce Jones",
+                Address = "2314 Pallet Street",
+                Email = "bryce@fakemail.com",
+                PhoneNumber = "223-305"
+            };
+            Order orderTwo = new Order()
+            {
+                Id = 2,
+                Name = "Hamdi Damdi",
+                Address = "152 Sundown Lane",
+                Email = "damdation@fakemail.com",
+                PhoneNumber = "123456"
+            };
+            Order orderThree = new Order()
+            {
+                Id = 3,
+                Name = "Enola Holmes",
+                Address = "1945 Victoria Court",
+                Email = "foundaclue@fakemail.com",
+                PhoneNumber = "2481632"
+            };
+            return Json(new {OrderOne = orderOne, OrderTwo = orderTwo,OrderThree = orderThree });
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            Order order = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return RedirectToAction("Error");
+            }
+            return View(order);
+        }
+
+        public IActionResult PrintOrders()
+        {
+            List<Order> dbOrders = StaticDb.Orders;
+            return View(dbOrders);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Homework01/SEDC.PizzaApp/SEDC.PizzaApp/StaticDb.cs
@@ -0,0 +1,60 @@
+﻿using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp
+{
+    public class StaticDb
+    {
+        public static List<Pizza> Pizzas = new List<Pizza>()
+        {
+            new Pizza()
+            {
+                Id = 1,
+                Name = "Margherita",
+                Price = 330,
+                IsOnPromotion = true,
+            },
+            new Pizza()
+            {
+                Id=2,
+                Name = "Capricciosa",
+                Price = 400,
+                IsOnPromotion = false
+            },
+            new Pizza()
+            {
+                Id = 3,
+                Name = "American Hot",
+                Price = 400,
+                IsOnPromotion = true
+            }
+        };
+
+        public static List<Order> Orders = new List<Order>()
+        {
+            new Order()
+            {
+                Id = 1,
+                Name = "Bryce Jones",
+                Address = "2314 Pallet Street",
+                Email = "bryce@fakemail.com",
+                PhoneNumber = "223-305"
+            },
+            new Order()
+            {
+                Id = 2,
+                Name = "Hamdi Damdi",
+                Address = "152 Sundown Lane",
+                Email = "damdation@fakemail.com",
+                PhoneNumber = "123456"
+            },
+            new Order()
+            {
+                Id = 3,
+                Name = "Enola Holmes",
+                Address = "1945 Victoria Court",
+                Email = "foundaclue@fakemail.com",
+                PhoneNumber = "2481632"
+            }
+        };
+    }
+}
